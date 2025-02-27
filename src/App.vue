f07e7df8fd6c507b5b06732242d6a88c90b4b5a3
Shakti <shaktisaxena2002@gmail.com>
2025-02-27 17:17:55 +0530

    formBuild

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 </script>
 
@@ -8,13 +8,14 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
+       <Test/>
     </div>
   </header>
 
-  <main>
-    <!-- <TheWelcome /> -->
-  </main>
+  <!-- <main>
+    <TheWelcome /> -->
+  <!-- </main> -->
 </template>
 
 <style scoped>
@@ -38,10 +39,10 @@ header {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
+  /* header .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
+  } */
 }
 </style>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -8,33 +8,61 @@
     </form>
 
     <ul>
-      <li v-for="(item, index) in data" v-bind:key="index">{{ item }}</li>
+      <li v-for="(item, index) in data" v-bind:key="index"><span>
+          {{ item }}
+        </span>
+        <a href="#" v-on:click.prevent="deleteItem(index)">Delete</a>
+        <a href="#" v-on:click.prevent="editItem(index)">Edit</a>
+      </li>
     </ul>
   </div>
 </template>
 <script>
+
+
 export default {
   name: "Test",
   data: () => {
     return {
       name: "",
-      data: []
+      data: [],
+      editIndex: false
     }
   },
 
   methods: {
     submit: function (e) {
       if (e.keyCode === 13) {
-        this.data.push(this.name);
-        this.name = "";
+        if (this.editIndex !== -1) {
+          this.data[this.editIndex] = this.name;
+          this.name = "";
+          this.editIndex = -1;
+        }
+        else {
+          this.data.push(this.name);
+          this.name = "";
+        }
       }
+    },
+    deleteItem: function (index) {
+      this.data.splice(index, 1)
+    },
+    editItem: function (index) {
+      this.editIndex = index;
+      this.name = this.data[index];
     }
   }
 }
 </script>
 <style>
-#testing li {
-  border: 1px solid red;
+#Test li {
+  border: 1px solid;
+  padding: 5px;
+}
+
+#Test li a {
+  font-size: 10px;
+  float: right;
   padding: 5px;
 }
 </style>
