6d360750dffd10c3350c3c9a33b4361b1e8f2784
Shakti <shaktisaxena2002@gmail.com>
2025-02-27 12:53:48 +0530

    taskk-1

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Test">
+    <h3>Hello {{ name }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "Test",
+  data: () => {
+    return {
+      name: "Shakti"
+    }
+  }
+}
+</script>
