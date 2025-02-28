bddc75f9caa5514eb53a9143150d7c07a0b08afb
Shakti <shaktisaxena2002@gmail.com>
2025-02-28 16:12:07 +0530

    practiceTimer

---
+++ src/components/Control.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Timer :count="count" />
+    <button v-on:click.prevent="increment()">Start</button>
+    <button v-on:click.prevent="stop()">Stop</button>
+  </div>
+</template>
+<script>
+import Timer from './Timer.vue';
+
+export default {
+  name: "Control",
+  components: { Timer },
+  data: function () {
+    return {
+      count: 0,
+      interval: null
+    }
+  },
+  methods: {
+    increment: function () {
+      this.interval = setInterval(() => { this.count++ }, 1000)
+    },
+    stop: function () {
+      clearInterval(this.interval)
+    }
+  }
+}
+</script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ count }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
