209e75c968ab05a8b57eecfe1ddae66269093737
Shakti <shaktisaxena2002@gmail.com>
2025-02-28 11:27:38 +0530

    BuildBtns

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+<script>
+import BaseBtn from './components/BaseBtn.vue';
+export default {
+  name: 'App',
+  components: {
+    BaseBtn
+  },
+  data() {
+    return {
+      name: "shakti"
+    }
+  },
+  methods: {
+    showAlert() {
+      alert('button clicked')
+    },
+    toggleRev() {
+      this.name = this.name.split('').reverse().join('');
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <!-- <TheWelcome /> -->
-  </main>
+  <BaseBtn :label="'Alert'" :action="showAlert" id="btn" />
+  <BaseBtn :label="'toggleName'" :action="toggleRev" id="btn" />
+  <p>name:{{ name }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#btn {
+  background-color: #0069D9;
+  color: #ffffff;
 }
 </style>
---
+++ src/components/BaseBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'btn',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    action: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      this.action();
+    }
+  }
+}
+
+</script>
